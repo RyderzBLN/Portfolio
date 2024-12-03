ba80d032dada8e95f35dc500fb36ca93114a3ef7
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-12-03 12:06:15 +0100

    finish header component design

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from './hero-section/hero-section.component';
+import { HeaderComponent } from "./shared/header/header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSectionComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Portfolio/src/app/hero-section/hero-section.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../shared/header/header.component';
+
+@Component({
+  selector: 'app-hero-section',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.scss'
+})
+export class HeroSectionComponent {
+
+}
