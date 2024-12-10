bc3e37ba568ee35699de6f0370cc10d9988d8ace
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-12-10 12:37:38 +0100

    finish projects section design

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeroSectionComponent } from './hero-section/hero-section.component';
 import { HeaderComponent } from "./shared/header/header.component";
 import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
 import { SkillSectionComponent } from "./skill-section/skill-section.component";
+import { ProjectsSectionComponent } from './projects-section/projects-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, HeaderComponent, AboutMeSectionComponent, SkillSectionComponent],
+  imports: [RouterOutlet, HeroSectionComponent, HeaderComponent, AboutMeSectionComponent, SkillSectionComponent, ProjectsSectionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- Portfolio/src/app/shared/header/header.component.ts
+++ Portfolio/src/app/shared/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
+
 export class HeaderComponent {
 
 }
