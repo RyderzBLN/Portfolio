7483eef0f5f7defb2b37e2642080f61e63f5c7d2
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-12-12 15:47:37 +0100

    finish carousel

--- Portfolio/src/app/reference-section/carousel/carousel.component.ts
+++ Portfolio/src/app/reference-section/carousel/carousel.component.ts
@@ -11,6 +11,10 @@ import { CommonModule } from '@angular/common';
 
 export class CarouselComponent {
   activeButton: string | null = null;
+  isCheckedItem1 = false;
+  isCheckedItem2 = false;
+  isCheckedItem3 = false;
+
 
   selectItem(itemId: string, event: Event): void {
     const item = document.getElementById(itemId) as HTMLInputElement;
@@ -18,12 +22,57 @@ export class CarouselComponent {
       item.checked = true;
     }
 
-    // Entferne die Klasse 'active' von allen Elementen
     const elements = document.querySelectorAll('.select');
     elements.forEach((el) => el.classList.remove('active'));
 
-    // Füge die Klasse 'active' zum angeklickten Element hinzu
     const element = event.target as HTMLElement;
     element.classList.add('active');
   }
-}
+
+  checkAndActivate() {
+    for (let i = 1; i <= 3; i++) {
+      const inputElement = document.getElementById(`item-${i}`) as HTMLInputElement;
+      const btnElement = document.getElementById(`btn-${i}`) as HTMLElement;
+
+      if (inputElement && inputElement.checked) {
+        btnElement.classList.add('active');
+      } else if (btnElement) {
+        btnElement.classList.remove('active');
+      }
+    }
+  }
+
+  nextItem() {
+    for (let i = 1; i <= 3; i++) {
+      const inputElement = document.getElementById(`item-${i}`) as HTMLInputElement;
+      if (inputElement && inputElement.checked) {
+        const nextIndex = (i % 3) + 1;
+        const nextInputElement = document.getElementById(`item-${nextIndex}`) as HTMLInputElement;
+        if (nextInputElement) {
+          nextInputElement.checked = true;
+          this.checkAndActivate();
+        }
+        break;
+      }
+    }
+  }
+
+  prevItem() {
+    for (let i = 1; i <= 3; i++) {
+      const inputElement = document.getElementById(`item-${i}`) as HTMLInputElement;
+      if (inputElement && inputElement.checked) {
+        const prevIndex = (i - 2 + 3) % 3 + 1;
+        const prevInputElement = document.getElementById(`item-${prevIndex}`) as HTMLInputElement;
+        if (prevInputElement) {
+          prevInputElement.checked = true;
+          this.checkAndActivate();
+        }
+        break;
+      }
+    }
+  }
+  
+
+  }
+  
+  
