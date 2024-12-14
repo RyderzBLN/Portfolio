250f50b2ccf41d04fac5c93e89f7d5fe75193b19
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-12-14 02:43:43 +0100

    finish contact me section design

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,18 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeroSectionComponent } from './hero-section/hero-section.component';
-import { HeaderComponent } from "./shared/header/header.component";
+import { HeaderComponent } from './shared/header/header.component';
 import { AboutMeSectionComponent } from './about-me-section/about-me-section.component';
-import { SkillSectionComponent } from "./skill-section/skill-section.component";
+import { SkillSectionComponent } from './skill-section/skill-section.component';
 import { ProjectsSectionComponent } from './projects-section/projects-section.component';
 import { ReferenceSectionComponent } from './reference-section/reference-section.component';
+import { ContactMeSectionComponent } from './contact-me-section/contact-me-section.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeroSectionComponent, HeaderComponent, AboutMeSectionComponent, SkillSectionComponent, ProjectsSectionComponent, ReferenceSectionComponent],
+  imports: [
+    RouterOutlet,
+    HeroSectionComponent,
+    HeaderComponent,
+    AboutMeSectionComponent,
+    SkillSectionComponent,
+    ProjectsSectionComponent,
+    ReferenceSectionComponent,
+    ContactMeSectionComponent
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'Portfolio';
--- Portfolio/src/app/reference-section/carousel/carousel.component.ts
+++ Portfolio/src/app/reference-section/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,16 +6,10 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
-
 export class CarouselComponent {
-  activeButton: string | null = null;
-  isCheckedItem1 = false;
-  isCheckedItem2 = false;
-  isCheckedItem3 = false;
-
-
+  
   selectItem(itemId: string, event: Event): void {
     const item = document.getElementById(itemId) as HTMLInputElement;
     if (item) {
@@ -31,7 +25,9 @@ export class CarouselComponent {
 
   checkAndActivate() {
     for (let i = 1; i <= 3; i++) {
-      const inputElement = document.getElementById(`item-${i}`) as HTMLInputElement;
+      const inputElement = document.getElementById(
+        `item-${i}`
+      ) as HTMLInputElement;
       const btnElement = document.getElementById(`btn-${i}`) as HTMLElement;
 
       if (inputElement && inputElement.checked) {
@@ -41,38 +37,4 @@ export class CarouselComponent {
       }
     }
   }
-
-  nextItem() {
-    for (let i = 1; i <= 3; i++) {
-      const inputElement = document.getElementById(`item-${i}`) as HTMLInputElement;
-      if (inputElement && inputElement.checked) {
-        const nextIndex = (i % 3) + 1;
-        const nextInputElement = document.getElementById(`item-${nextIndex}`) as HTMLInputElement;
-        if (nextInputElement) {
-          nextInputElement.checked = true;
-          this.checkAndActivate();
-        }
-        break;
-      }
-    }
-  }
-
-  prevItem() {
-    for (let i = 1; i <= 3; i++) {
-      const inputElement = document.getElementById(`item-${i}`) as HTMLInputElement;
-      if (inputElement && inputElement.checked) {
-        const prevIndex = (i - 2 + 3) % 3 + 1;
-        const prevInputElement = document.getElementById(`item-${prevIndex}`) as HTMLInputElement;
-        if (prevInputElement) {
-          prevInputElement.checked = true;
-          this.checkAndActivate();
-        }
-        break;
-      }
-    }
-  }
-  
-
-  }
-  
-  
+}
