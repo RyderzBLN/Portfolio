ddd478a7a233f668adad174f0395cb55c1be6f45
Murat Catili <158097420+RyderzBLN@users.noreply.github.com>
2024-12-18 21:19:11 +0100

    before translate

--- Portfolio/src/app/shared/header/header.component.ts
+++ Portfolio/src/app/shared/header/header.component.ts
@@ -1,13 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 
 export class HeaderComponent {
 
+
+  isDE: boolean = true; // Standardmäßig Deutsch aktiv
+
+  toggleLanguage() {
+    this.isDE = !this.isDE; // Einfacher Wechsel zwischen true und false
+  }
 }
